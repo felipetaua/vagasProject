18f798e60391860d4154e40a8ac4aad5f8f4f81c
felipetaua <taua.felipee@gmail.com>
2025-08-23 01:24:14 -0300

    testando se o header vai aparecer a imagem

--- authenticated/templates/header.php
+++ authenticated/templates/header.php
@@ -17,35 +17,37 @@ $headerResult = $headerStmt->get_result();
 $headerUser = $headerResult->fetch_assoc();
 $headerStmt->close();
 ?>
-<header style='background:white; margin-top:-10px; padding:5px;'>
+<!-- Adicione este link no <head> da sua página principal (ex: ultimasVagas.php): -->
+<link rel="stylesheet" href="/sistemaDeVagas/css/header.css">
+<header class="main-header">
     <ul>
-        <a href='./home.php'> <li>
-            <img src='../imagens/Logo.svg' alt='Logo Jobs In Cariri' class='logo'> JOBS IN CARIRI
+        <a href='/sistemaDeVagas/authenticated/home.php'> <li>
+            <img src='/sistemaDeVagas/imagens/Logo.svg' alt='Logo Jobs In Cariri' class='logo'> JOBS IN CARIRI
         </li></a> 
-        <a href='./profissionais.php'><li>Profissionais</li></a>
-        <a href='./cadastroVagas.php'><li>Cadastrar vaga</li></a>
-        <a href='./ultimasVagas.php'><li>Últimas vagas</li></a>
-        <a href='./vagasCriadas.php'><li>Minhas vagas</li></a>
+        <a href='/sistemaDeVagas/authenticated/profissionais.php'><li>Profissionais</li></a>
+        <a href='/sistemaDeVagas/authenticated/cadastroVagas.php'><li>Cadastrar vaga</li></a>
+        <a href='/sistemaDeVagas/authenticated/ultimasVagas.php'><li>Últimas vagas</li></a>
+        <a href='/sistemaDeVagas/authenticated/vagasCriadas.php'><li>Minhas vagas</li></a>
         <div class='dropdown'> 
-            <div class='perfil-img' style='display:flex; align-items:center; justify-content:center;'>
-                <div style='display:flex; flex-direction:column; align-items:center;'>
+            <div class='perfil-img'>
+                <div>
                     <?php if ($headerUser && !empty($headerUser['foto'])): ?>
-                        <img src='../uploads/<?php echo htmlspecialchars($headerUser['foto']); ?>' style='width:50px; height:50px; border-radius:100%;' alt="Foto de perfil">
+                        <img src='/sistemaDeVagas/uploads/<?php echo htmlspecialchars($headerUser['foto']); ?>' class="perfil-foto" alt="Foto de perfil">
                     <?php else: ?>
-                        <img src='https://placehold.co/50x50' style='width:50px; height:50px; border-radius:100%;' alt="Foto de perfil padrão">
+                        <img src='https://placehold.co/50x50' class="perfil-foto" alt="Foto de perfil padrão">
                     <?php endif; ?>
                 </div>    
                 <li class='dropdown-btn'><?php echo $headerUser ? htmlspecialchars($headerUser['nome']) : 'Usuário'; ?></li>
                 <svg xmlns='http://www.w3.org/2000/svg' style='width:10px; color:green;' viewBox='0 0 320 512'><path d='M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z'/ ></svg>
             </div>
             <ul class='dropdown-menu'>
-                <a href='perfil.php'><li>Editar perfil</li></a>
-                <a href='ranking.php'> <li>Ranking</li></a>
-                <a href='./profissao.php'> <li>Profissão</li></a>
-                <a href='contratos.php'><li>Contratos</li></a>
+                <a href='/sistemaDeVagas/authenticated/perfil.php'><li>Editar perfil</li></a>
+                <a href='/sistemaDeVagas/authenticated/ranking.php'> <li>Ranking</li></a>
+                <a href='/sistemaDeVagas/authenticated/profissao.php'> <li>Profissão</li></a>
+                <a href='/sistemaDeVagas/authenticated/contratos.php'><li>Contratos</li></a>
                 <a href='#'> <li>Chat</li></a>
-                <a href='curriculo.php'> <li>Currículo</li></a>
-                <a href='./logout.php'><li>Sair</li></a>
+                <a href='/sistemaDeVagas/authenticated/curriculo.php'> <li>Currículo</li></a>
+                <a href='/sistemaDeVagas/authenticated/logout.php'><li>Sair</li></a>
             </ul>
         </div>
     </ul>
