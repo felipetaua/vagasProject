23eb77c269557134af0204ebf062a60a561c0ec5
felipetaua <taua.felipee@gmail.com>
2025-08-25 10:08:59 -0300

    ajustando a conexão com banco de dado e ajustando o header, o ultimo foi ajuste css

--- authenticated/db_connection.php
+++ authenticated/db_connection.php
@@ -1,22 +1,12 @@
 <?php
-// c:\xampp\htdocs\sistemaDeVagas\authenticated\db_connection.php
-
-// Centralized database connection file
-
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "jobs";
 
-// Create a new mysqli connection object
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check for connection errors
 if ($conn->connect_error) {
-    // In a real production environment, you would log this error and show a generic message.
-    error_log("Database Connection Failed: " . $conn->connect_error);
-    die("Falha na conexão com o banco de dados. Por favor, tente novamente mais tarde.");
+    die("Falha na conexão: " . $conn->connect_error);
 }
-
-// Set the character set to utf8mb4 for full Unicode support, preventing encoding issues.
 $conn->set_charset("utf8mb4");
+?>
--- authenticated/templates/header.php
+++ authenticated/templates/header.php
@@ -1,53 +1,35 @@
 <?php
-// This header template assumes that a session has been started
-// and a database connection ($conn) is available.
-
-if (!isset($_SESSION["user_id"])) {
-    // This is a fallback, the main page should handle this first.
-    header("Location: ../login.php");
-    exit;
-}
-$headerUserId = $_SESSION['user_id'];
-
-// Fetch user data for the header
-$headerStmt = $conn->prepare("SELECT nome, foto FROM `cadastro` WHERE id = ?");
-$headerStmt->bind_param("i", $headerUserId);
-$headerStmt->execute();
-$headerResult = $headerStmt->get_result();
-$headerUser = $headerResult->fetch_assoc();
-$headerStmt->close();
+// Este arquivo assume que a variável $user com 'nome' e 'foto' foi definida antes de incluí-lo.
 ?>
-<!-- Adicione este link no <head> da sua página principal (ex: ultimasVagas.php): -->
-<link rel="stylesheet" href="/sistemaDeVagas/css/header.css">
-<header class="main-header">
+<header style='background:white; margin-top:-10px; padding:5px;'>
     <ul>
-        <a href='/sistemaDeVagas/authenticated/home.php'><li>
+        <a href='/sistemaDeVagas/authenticated/home.php'> <li>
             <img src='/sistemaDeVagas/imagens/Logo.svg' alt='Logo Conexão RH 2.0' class='logo'> Conexão RH 2.0
-        </li></a>
+        </li></a> 
         <a href='/sistemaDeVagas/authenticated/profissionais.php'><li>Profissionais</li></a>
         <a href='/sistemaDeVagas/authenticated/cadastroVagas.php'><li>Cadastrar vaga</li></a>
         <a href='/sistemaDeVagas/authenticated/ultimasVagas.php'><li>Últimas vagas</li></a>
         <a href='/sistemaDeVagas/authenticated/vagasCriadas.php'><li>Minhas vagas</li></a>
         <div class='dropdown'> 
-            <div class='perfil-img'>
-                <div>
-                    <?php if ($headerUser && !empty($headerUser['foto'])): ?>
-                        <img src='/uploads/$row[foto]' style='width:50px; height:50px; border-radius:100%;'> 
+            <div class='perfil-img' style='display:flex; align-items:center; justify-content:center;'>
+                <div style='display:flex; flex-direction:column; align-items:center;'>
+                    <?php if (isset($user) && !empty($user['foto'])): ?>
+                        <img src='/sistemaDeVagas/authenticated/uploads/<?php echo htmlspecialchars($user['foto']); ?>' style='width:50px; height:50px; border-radius:100%;'>
                     <?php else: ?>
-                        <img src='https://placehold.co/50x50' class="perfil-foto" alt="Foto de perfil padrão">
+                        <img src='https://placehold.co/50x50' alt='Foto de perfil padrão' style='width:50px; height:50px; border-radius:100%;'>
                     <?php endif; ?>
                 </div>    
-                <li class='dropdown-btn'><?php echo $headerUser ? htmlspecialchars($headerUser['nome']) : 'Usuário'; ?></li>
+                <li class='dropdown-btn'><?php echo (isset($user) && $user) ? htmlspecialchars($user['nome']) : 'Perfil'; ?></li>
                 <svg xmlns='http://www.w3.org/2000/svg' style='width:10px; color:green;' viewBox='0 0 320 512'><path d='M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z'/ ></svg>
             </div>
             <ul class='dropdown-menu'>
-                <a href='/sistemaDeVagas/authenticated/perfil.php'><li>Editar perfil</li></a>
-                <a href='/sistemaDeVagas/authenticated/ranking.php'> <li>Ranking</li></a>
+                <a href='perfil.php'><li>Editar perfil</li></a>
+                <a href='#'> <li>Ranking</li></a>
                 <a href='/sistemaDeVagas/authenticated/profissao.php'> <li>Profissão</li></a>
-                <a href='/sistemaDeVagas/authenticated/contratos.php'><li>Contratos</li></a>
+                <a href='#'><li>Contratos</li></a>
                 <a href='#'> <li>Chat</li></a>
-                <a href='/sistemaDeVagas/authenticated/curriculo.php'> <li>Currículo</li></a>
-                <a href='/sistemaDeVagas/authenticated/logout.php'><li>Sair</li></a>
+                <a href='curriculo.php'> <li>Currículo</li></a>
+                <a href='./logout.php'><li>Sair</li></a>
             </ul>
         </div>
     </ul>
