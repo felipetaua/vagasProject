c01e592872b93df534486bd754950d3982426054
felipetaua <taua.felipee@gmail.com>
2025-08-26 19:39:56 -0300

    adicionando a interatividade do toggle button

--- authenticated/profissao.php
+++ authenticated/profissao.php
@@ -134,6 +134,24 @@ if ($result_professions->num_rows > 0) {
         var formContainer = document.getElementById('add-profession-container');
         formContainer.classList.toggle('hidden');
     });
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const dropdownBtn = document.querySelector('.dropdown-btn');
+        const dropdownMenu = document.querySelector('.dropdown-menu');
+
+        if (dropdownBtn && dropdownMenu) {
+            dropdownBtn.addEventListener('click', (event) => {
+                event.stopPropagation(); // Impede que o clique se propague e feche o menu imediatamente.
+                dropdownMenu.classList.toggle('show');
+            });
+
+            window.addEventListener('click', (event) => {
+                if (!dropdownBtn.contains(event.target)) {
+                    dropdownMenu.classList.remove('show');
+                }
+            });
+        }
+    });
   </script>
 </body>
 </html>
