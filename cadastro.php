44742d5b403bc67f0fc66a8d8324880d14c7c71d
felipetaua <taua.felipee@gmail.com>
2025-08-26 09:49:14 -0300

    melhorando a interface deixando mais profissional, adicionando a interatividade

--- cadastro.php
+++ cadastro.php
@@ -1,219 +1,106 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro Profissional</title>
     <link rel="stylesheet" href="./css/styleCadastro.css">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <script src="./js/cadastro.js"></script>
-    <title>Cadastro</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="imagem-cadastro">
-        <h1>Encontre seu lugar no mundo do trabalho</h1>
-        <h1 class="subtitle">O caminho para o seu sucesso profissional começa aqui!</h1>
+    <div class="main-container">
+        <div class="info-column">
+            <h1>Encontre seu lugar no mundo do trabalho</h1>
+            <p>O caminho para o seu sucesso profissional começa aqui!</p>
+        </div>
+
+        <div class="form-column">
+            <form id="regForm" action="validaCadastro.php" method="POST" novalidate>
+                
+                <div class="progress-indicator">
+                    <div class="step active">
+                        <span class="step-number">1</span>
+                        <span class="step-label">Cadastro</span>
+                    </div>
+                    <div class="step">
+                        <span class="step-number">2</span>
+                        <span class="step-label">Dados</span>
+                    </div>
+                    <div class="step">
+                        <span class="step-number">3</span>
+                        <span class="step-label">Finalizar</span>
+                    </div>
+                </div>
+
+                <div class="tab-content">
+                    <h2>Informações de Acesso</h2>
+                    <div class="input-group">
+                        <label for="nome">Nome</label>
+                        <input type="text" placeholder="Digite seu nome" name="nome" required>
+                        <span class="error-message"></span>
+                    </div>
+                    <div class="input-group">
+                        <label for="sobrenome">Sobrenome</label>
+                        <input type="text" placeholder="Digite seu sobrenome" name="sobrenome" required>
+                        <span class="error-message"></span>
+                    </div>
+                    <div class="input-group">
+                        <label for="email">E-mail</label>
+                        <input type="email" placeholder="seu.email@exemplo.com" name="email" required>
+                        <span class="error-message"></span>
+                    </div>
+                    <div class="input-group">
+                        <label for="senha">Senha</label>
+                        <input type="password" placeholder="Crie uma senha forte" name="senha" id="senha" required>
+                        <span class="error-message"></span>
+                    </div>
+                    <div class="input-group">
+                        <label for="confirmaSenha">Confirmar Senha</label>
+                        <input type="password" placeholder="Confirme sua senha" name="confirmaSenha" required>
+                        <span class="error-message"></span>
+                    </div>
+                </div>
+
+                <div class="tab-content">
+                    <h2>Dados Pessoais</h2>
+                    <div class="input-group">
+                        <label for="cpf">CPF</label>
+                        <input type="text" placeholder="000.000.000-00" name="cpf" required>
+                        <span class="error-message"></span>
+                    </div>
+                    <div class="input-group">
+                        <label for="dtNascimento">Data de Nascimento</label>
+                        <input type="date" name="dtNascimento" required>
+                        <span class="error-message"></span>
+                    </div>
+                    <div class="input-group">
+                        <label for="celular">Telefone / Celular</label>
+                        <input type="tel" placeholder="(00) 90000-0000" name="celular" required>
+                        <span class="error-message"></span>
+                    </div>
+                </div>
+
+                <div class="tab-content">
+                    <h2>Termos de Uso</h2>
+                    <div class="terms-box">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, illum libero pariatur incidunt culpa, ex mollitia dolore porro ducimus temporibus amet ea corporis ad optio error animi repell at sed aspernatur quod!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, saepe. Amet, reiciendis. Doloremque, molestiae.</p>
+                    </div>
+                    <div class="input-group checkbox-group">
+                        <input type="checkbox" name="termos" id="termos" required>
+                        <label for="termos">Eu li e aceito os termos de uso.</label>
+                        <span class="error-message"></span>
+                    </div>
+                </div>
+
+                <div class="navigation-buttons">
+                    <button type="button" id="prevBtn">Anterior</button>
+                    <button type="button" id="nextBtn">Próximo</button>
+                </div>
+            </form>
+        </div>
     </div>
-    
-<div class="tab-container">
-<form action="validaCadastro.php" method="POST">
-  <div class="tab">
-    <button class="tablinks active" onclick="openTab(event, 'tab1')">Cadastro inicial</button>
-    <button class="tablinks" onclick="openTab(event, 'tab2')">Dados pessoais</button>
-    <button class="tablinks" onclick="openTab(event, 'tab3')">Finalização</button>
-  </div>
-  <div id="tab1" class="tab-content active">
-    
-      <div class="cadastro-inicial">
-        <h1>Cadastro inicial</h1>
-        <div class="caixa-input">
-        <label for="nome">Nome</label>
-        <input type="text" placeholder="Digite seu nome" name="nome" >
-        </div>
-        <div class="caixa-input">
-        <label for="sobrenome">Sobrenome</label>
-        <input type="text" placeholder="Digite seu sobrenome" name="sobrenome">
-        </div>
-        <div class="caixa-input">
-        <label for="email">Email</label>
-        <input type="email" placeholder ="Digite seu email" name="email" required></div>
-        <div class="caixa-input">
-        <label for="senha">Senha</label>
-        <input type="password" placeholder="Crie uma senha" name="senha"></div>
-        <div class="caixa-input">
-        <label for="confirmar-senha">Confirmar senha</label>
-        <input type="password" placeholder="Confirme sua senha" name="confirmaSenha">
 
-</div>
-<input type="button" value="Próximo" onclick="nextTab()">        
-        </div>
-  </div>
-  <div id="tab2" class="tab-content">
-  
-    <div id="dadosPessoais">
-        <h1>Dados pessoais</h1>
-        <div id="caixa-input">
-        <label for="cpf">CPF</label>
-        <input type="text" placeholder="CPF" name="cpf">
-        </div>
-        <div id="caixa-input">
-          <label for="rg">RG</label>
-        <input type="text" placeholder="RG" name="rg">
-        </div>
-        <div id="caixa-input">
-          <label for="dtNascimento">Data de nascimento</label>
-        <input type="date" placeholder="Data de nascimento" name="dtNascimento"></div>
-        <div id="caixa-input">
-          <label for="endereco">Endereço</label>
-        <input type="text" placeholder ="Digite endereço" name="endereco">
-        </div>
-        <div id="caixa-input">
-          <label for="cidade">Cidade</label> 
-        <input type="text" placeholder="Digite o nome da sua cidade" name="cidade">
-        </div>
-        <div id="caixa-input">
-          <label for="celular">Telefone</label>
-        <input type="tel" placeholder="Digite o numero de telefone" name="celular">
-        </div>
-        
-        <div class="btnsDadosPessoais">
-        <button onclick="prevTab()" class="btnAnteriorDadosPessoais" style="background:red; margin:5px;">Anterior</button>
-        <input type="button" value="Próximo" onclick="nextTab()" style="margin:5px;">
-        </div>
-        </div>
-  </div>
-  <div id="tab3" class="tab-content">
-    <h1>Termos de uso</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum libero pariatur incidunt culpa, ex 
-      mollitia dolore porro ducimus temporibus amet ea corporis ad optio error animi repell
-      at sed aspernatur quod!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum libero pariatur incidunt culpa, ex 
-      mollitia dolore porro ducimus temporibus amet ea corporis ad optio error animi repell
-      at sed aspernatur quod!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum libero pariatur incidunt culpa, ex 
-      mollitia dolore porro ducimus temporibus amet ea corporis ad optio error animi repell
-      at sed aspernatur quod!</p>
-      <div class="check-termos-de-uso">
-      <input type="checkbox" name="meucheckbox"></div>
-      <div class="btn-termos-de-uso">
-    <button onclick="prevTab()">Anterior</button>
-    <input type="submit" value="Finalizar cadastro" onclick="nextTab()">
-    </div>
-    
-  </div>
-  </form>
-</div>
-
-
+    <script src="./js/cadastro.js"></script>
 </body>
 </html>
-
-
-
-<style>
-  .tab-content h1{
-    color:white;
-    text-align:center;
-  }
-
-  .tab-content p{
-    color:white;
-    
-    font-size:16px;
-    line-height:1.5;
-    
-
-  }
-  .btn-termos-de-uso{
-    display:flex;
-    align-items:center;
-    justify-content:center;
-  }
-  .btn-termos-de-uso input[type="submit"]{
-    background-color: green;
-        color: white;
-        font-size:16px;
-        width:180px;
-        height:35px;
-        border-radius: 5px;
-        border:none;
-        outline: none;
-        padding-left: 5px;
-        margin:10px;
-
-  }
-
-  .check-termos-de-uso {
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    margin:auto;
-  }
-
-
-  .btn-termos-de-uso button{
-    background-color: #FFA500;
-        color: white;
-        font-size:16px;
-        width:180px;
-        height:35px;
-        border-radius: 5px;
-        border:none;
-        outline: none;
-        padding-left: 5px;
-        margin:10px;
-  }
-  #dadosPessoais #caixa-input{
-        display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    margin:10px;
-      }
-
-#dadosPessoais #caixa-input label{
-  color:white;
-}
-
-#dadosPessoais{ display:flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items:center;
-    margin:20px auto;}
-
-    #dadosPessoais input{
-      width:400px;
-      height:35px;
-      border-radius: 5px;
-      border:none;
-      outline: none;
-      padding-left: 5px;
-      margin:0;
-    }
-
-    #dadosPessoais h1{
-        color:white;
-    }
-
-    #dadosPessoais input[type="button"]{
-        background-color: #FFA500;
-        color: white;
-        font-size:16px;
-        width:180px;
-      }
-
-     
-      .btnAnteriorDadosPessoais{
-        background-color: #FFA500;
-        color: white;
-        font-size:16px;
-        width:180px;
-        height:35px;
-       
-        border-radius: 5px;
-        border:none;
-        outline: none;
-        padding-left: 5px
-      }
-
-    .btnsDadosPessoais{display:flex; align-items:center;}
-</style>
--- js/cadastro.js
+++ js/cadastro.js
@@ -1,81 +1,126 @@
-var currentTab = 0;
-showTab(currentTab);
+document.addEventListener("DOMContentLoaded", function() {
+    let currentTab = 0;
+    const tabs = document.querySelectorAll(".tab-content");
+    const steps = document.querySelectorAll(".step");
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
 
-function showTab(n) {
-  var i;
-  var tabcontent = document.getElementsByClassName("tab-content");
-  var tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-    tablinks[i].classList.remove("active");
-  }
-  tabcontent[n].style.display = "block";
-  tablinks[n].classList.add("active");
-  if (n == 0) {
-    document.getElementById("prevBtn").style.display = "none";
-  } else {
-    document.getElementById("prevBtn").style.display = "inline";
-  }
-  if (n == tabcontent.length - 1) {
-    document.getElementById("nextBtn").innerHTML = "Concluir";
-  } else {
-    document.getElementById("nextBtn").innerHTML = "Próximo";
-  }
-}
-
-function nextTab() {
-  var tabcontent = document.getElementsByClassName("tab-content");
-  var inputs = tabcontent[currentTab].getElementsByTagName("input");
-  var valid = true;
-  var emailInput;
-  
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].type === "checkbox" && !inputs[i].checked) { 
-      valid = false;
-      break;
-    } else if (inputs[i].name === "email") { 
-      emailInput = inputs[i];
-    } else if (inputs[i].value === "" || inputs[i].value === undefined) { 
-      valid = false;
-      break;
-    }
-  }
-  
-  if (emailInput && emailInput.type === "email" && !isValidEmail(emailInput.value)) {
-    alert("Por favor, insira um endereço de email válido.");
-  } else if (valid) {
-    if (currentTab < tabcontent.length - 1) {
-      currentTab++;
-      showTab(currentTab);
-    } else {
-      var lastTabInputs = tabcontent[currentTab].getElementsByTagName("input");
-      var checkBoxSelected = false;
-      for (var j = 0; j < lastTabInputs.length; j++) {
-        if (lastTabInputs[j].type === "checkbox" && lastTabInputs[j].checked) {
-          checkBoxSelected = true;
-          break;
-        }
-      }
-      if (checkBoxSelected) {
-        alert("Cadastro finalizado");
-      } else {
-        alert("Por favor, selecione o checkbox antes de prosseguir.");
-      }
-    }
-  } else {
-    alert("Verifique se todos os campos foram preenchidos!");
-  }
-}
-
-
-function prevTab() {
-  if (currentTab > 0) {
-    currentTab--;
     showTab(currentTab);
-  }
-}
 
-function isValidEmail(email) {
-  var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailPattern.test(email);
-}
+    function showTab(n) {
+        // Exibe a aba atual e oculta as outras
+        tabs.forEach((tab, index) => {
+            tab.style.display = (index === n) ? "block" : "none";
+        });
+
+        // Atualiza o indicador de progresso
+        updateStepIndicator(n);
+
+        // Configura os botões
+        prevBtn.style.display = (n === 0) ? "none" : "inline";
+        nextBtn.innerHTML = (n === tabs.length - 1) ? "Finalizar Cadastro" : "Próximo";
+
+        // Se for a última aba, o botão vira "submit"
+        if (n === tabs.length - 1) {
+            nextBtn.type = "submit";
+        } else {
+            nextBtn.type = "button";
+        }
+    }
+
+    function navigate(n) {
+        // Valida a aba atual antes de prosseguir
+        if (n === 1 && !validateForm()) return false;
+
+        // Esconde a aba atual e avança
+        tabs[currentTab].style.display = "none";
+        currentTab = currentTab + n;
+
+        if (currentTab >= tabs.length) {
+            // Se chegou ao fim, submete o formulário
+            document.getElementById("regForm").submit();
+            return false;
+        }
+        showTab(currentTab);
+    }
+    
+    function updateStepIndicator(n) {
+        steps.forEach((step, index) => {
+            if (index < n) {
+                step.classList.add("active"); // Etapas passadas
+            } else if (index === n) {
+                step.classList.add("active"); // Etapa atual
+            } else {
+                step.classList.remove("active"); // Etapas futuras
+            }
+        });
+    }
+
+    function validateForm() {
+        let valid = true;
+        const currentTabInputs = tabs[currentTab].querySelectorAll("input[required]");
+
+        currentTabInputs.forEach(input => {
+            // Limpa erros anteriores
+            clearError(input);
+
+            // Validação genérica para campos vazios
+            if (input.value.trim() === "" && input.type !== 'checkbox') {
+                showError(input, "Este campo é obrigatório.");
+                valid = false;
+            }
+            // Validação para checkbox
+            if (input.type === 'checkbox' && !input.checked) {
+                showError(input, "Você precisa aceitar os termos.");
+                valid = false;
+            }
+            // Validação para email
+            if (input.type === 'email' && input.value.trim() !== "" && !isValidEmail(input.value)) {
+                showError(input, "Por favor, insira um e-mail válido.");
+                valid = false;
+            }
+            // Validação para confirmação de senha
+            if (input.name === 'confirmaSenha') {
+                const senha = document.getElementById('senha').value;
+                if (input.value !== senha) {
+                    showError(input, "As senhas não coincidem.");
+                    valid = false;
+                }
+            }
+        });
+
+        return valid;
+    }
+
+    function showError(input, message) {
+        const inputGroup = input.parentElement;
+        const errorSpan = inputGroup.querySelector(".error-message");
+        input.classList.add("invalid");
+        if (errorSpan) {
+            errorSpan.textContent = message;
+        }
+    }
+
+    function clearError(input) {
+        const inputGroup = input.parentElement;
+        const errorSpan = inputGroup.querySelector(".error-message");
+        input.classList.remove("invalid");
+        if (errorSpan) {
+            errorSpan.textContent = "";
+        }
+    }
+
+    function isValidEmail(email) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    // Adiciona os eventos aos botões
+    prevBtn.addEventListener("click", () => navigate(-1));
+    nextBtn.addEventListener("click", () => {
+        if (nextBtn.type !== 'submit') {
+            navigate(1);
+        }
+    });
+
+});
